5e23318145bfc3a0a39b8ef98b8f89f06e670737
Abdulaziz_c5 <abdulazizbekabdusodiqov@gmail.com>
2025-01-13 15:18:30 +0500

    create project

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require("sequelize");
+const config = require("config")
+
+
+const sequelize = new Sequelize(
+    config.get("db_name"),
+    config.get("db_username"),
+    config.get("db_password"),
+    {
+        dialect : "postgres",
+        logging : true,
+        port: config.get("db_port"),
+        port: config.get("db_host")
+    }
+)
+
+module.exports = sequelize;
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const config = require("config")
+const sequelize = require("./config/db")
+
+const PORT = config.get("port")
+
+const app = express()
+
+app.use(express.json())
+
+async function start() {
+    try {
+        
+        await sequelize.authenticate()
+        await sequelize.sync({alter:true})
+
+        app.listen(PORT,()=>{
+            console.log("Server running at http://localhost:"+PORT)
+        })      
+    } catch (error) {
+        console.log(error);
+    }
+};
+start();
